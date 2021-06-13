0172be7da4ff464d6d7dd3f8cf185ef99dd23156
Zulham Ari <ari.comunity1@gmail.com>
2021-06-13 08:09:36 +0700

    update navbar

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 function getData() {
     var click = document.getElementById("list-drop");
     if (click.style.display ==="none") {
-        click.style.display = "block";
+        click.style.display = "flex";
+        click.style.flexDirection = "column";
     } else {
         click.style.display = "none";
     }
