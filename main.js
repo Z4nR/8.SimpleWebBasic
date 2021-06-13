131896860bd6198d0c7d45946bf4ca2ae5c8fdc8
Zulham Ari <ari.comunity1@gmail.com>
2021-06-13 20:15:00 +0700

    update content

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 function getData() {
     var click = document.getElementById("list-drop");
     if (click.style.display ==="none") {
-        click.style.display = "flex";
-        click.style.flexDirection = "column";
+        click.style.display = "block";
     } else {
         click.style.display = "none";
+    }
+}
+
+function hiddenList() {
+    var list = document.getElementsById("list-drop");
+    if (list.style.display === "inline-block") {
+        list.style.display = "none";
+    } else {
+        list.style.display = "inline-block";
     }
 }
